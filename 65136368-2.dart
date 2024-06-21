03ce3f2013d4f0f1dff151c21f8e343539836828
winnie2205-2 <wasinee.pw@gmail.com>
2024-06-21 11:57:13 +0700

    Add files via upload

---
+++ 65136368-2.dart
@@ -0,0 +1,13 @@
+void main() {
+  int number = 22;  
+  if (number % 2 == 0) {
+    print('$number is even.');
+  } else {
+    print('$number is odd.');
+  }
+  
+  for (int i = 1; i <= 5; i++) {
+    print('Number: $i');
+  }
+}
+
